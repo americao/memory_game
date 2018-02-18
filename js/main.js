f9b119f689f194279e94d105bf22705bb267a3ae
Ortiz <aortiz@eism-am02042682.attlocal.net>
2018-02-17 16:08:21 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = []; 
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+
+var flipCard = function (cardId) {
+	console.log("User Flipped " + cards[cardId]);
+	
+	cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+};
+
+flipCard(0);
+flipCard(2);
+
+
