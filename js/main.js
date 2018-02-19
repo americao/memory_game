52f25c89f63187e1b0710c5814b9769d0c9b09b0
Ortiz <aortiz@eism-am02042682.attlocal.net>
2018-02-19 12:12:59 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{	
+	rank:  'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-ofdiamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamond',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 
 var cardsInPlay = []; 
 
@@ -11,9 +32,13 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-	console.log("User Flipped " + cards[cardId]);
+	console.log("User Flipped " + cards[cardId].rank);
 	
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+
+	console.log(cards[cardId].suit);
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();
